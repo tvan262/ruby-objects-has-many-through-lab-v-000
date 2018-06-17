29ee71ad471066b909afd63633e9bbc9007962ad
Tuong Van Nguyen <tvan262@yahoo.com>
2018-06-17 08:48:40 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,5 @@
 class Patient
+
   attr_accessor :name
 
   def initialize(name)
@@ -6,18 +7,18 @@ class Patient
     @appointments = []
   end
 
-  def add_appointment(appointment)
-    @appointments << appointment
-    appointment.patient = self
-  end
+   def add_appointment(appointment)
+     @appointments << appointment
+     appointment.patient = self
+   end
 
-  def appointments
-    @appointments
-  end
+   def appointments
+     @appointments
+   end
 
-  def doctors
-    @appointments.collect do |appointment|
-      appointment.doctor
-    end
-  end
-end
+   def doctors
+     @appointments.map do |appointment|
+       appointment.doctor
+     end
+   end
+ end
